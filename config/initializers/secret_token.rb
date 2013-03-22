6a025f3cc4f5c9c46fa8b3c61f075135200fcd10
idlexp <idlexp@yahoo.co.uk>
2013-03-22 17:49:28 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'eb6b4c3131e8202529a9711c8126d787b162b15ceb2ff43f802293b849da1b1b7ddf6b76c3540fefa12075beb6bc7ca7ec57413a37b21c26f98527e155842519'
